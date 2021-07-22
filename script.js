bafa4a5fefc394b31d489fb180df920f8c28c092
shebiraaja <71530161+shebiraaja@users.noreply.github.com>
2021-07-22 11:03:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+const clear = document.querySelector(".clear")
+const list = document.querySelector(".list")
+const input = document.querySelector("#input")
+
+let LIST = []
+let id = 0;
+
+const check = "fa-check-circle"
+const uncheck = "fa-circle"
+const lineThrough = "lineThrough"
+
+const data = localStorage.getItem("TODO")
+
+if(data){
+    LIST = JSON.parse(data)
+    loadTodos(LIST)
+    id = LIST.length;
+}else{
+    LIST = [];
+    id = 0;
+}
+
+function loadTodos(array){
+    array.forEach(function(index){
+        addTodo(index.toDo, index.id, index.done, index.trash)
+    })
+}
+
+clear.addEventListener("click", function(){
+    localStorage.clear()
+    location.reload()
+})
+
+
+
+
+function addTodo(toDo, id, done, trash){
+
+    if(trash){return;}
+
+    const TRUE = done ? check : uncheck
+    const LINE = done ? lineThrough : ""
+    
+
+    let text = `
+    <li class="item">
+        <i class=" far ${TRUE}" id="${id}" job="complete"></i>
+        <p class="text ${LINE}">${toDo}</p>
+        <i class="fas fa-trash-alt" id="${id}" job="delete"></i>
+    </li> 
+    `
+
+    let position = "beforeend"
+
+    list.insertAdjacentHTML(position, text)
+    
+}
+
+
+
+//key press
+
+document.addEventListener("keypress", function(event){
+    if(event.keyCode == 13){
+        const toDo = input.value;
+
+        if(toDo){
+            addTodo(toDo, id, false, false)
+            LIST.push({
+                toDo : toDo,
+                id : id,
+                done : false,
+                trash : false,
+            })
+            id++;
+            input.value = ""
+        }
+        localStorage.setItem("TODO", JSON.stringify(LIST))
+    }     
+})
+
+
+function completeTodos(element){
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
+    element.parentNode.querySelector(".text").classList.toggle(lineThrough)
+    LIST[element.id].done = LIST[element.id].done ? false : true
+}
+
+function removeTodo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode)
+    LIST[element.id].trash = true
+}
+
+list.addEventListener("click", function(event){
+    let element = event.target;
+    let elementJOB = event.target.attributes.job.value;
+
+    if(elementJOB == "complete"){
+        completeTodos(element)
+    }else if(elementJOB == "delete"){
+        removeTodo(element)
+    }
+    localStorage.setItem("TODO", JSON.stringify(LIST))
+})
+
+localStorage.setItem("TODO", JSON.stringify(LIST))
